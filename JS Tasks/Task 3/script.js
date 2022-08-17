32dcbb1b0bd06ecde569cc557bba58d0a2eb74f6
Arturas <mixmln14@gmail.com>
2022-08-17 16:01:09 +0300

    Task 3 done

--- JS Tasks/Task 3/script.js
+++ JS Tasks/Task 3/script.js
@@ -11,4 +11,37 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+
+const btnEl = document.getElementById("btn");
+const outputEl = document.getElementById("output");
+
+async function getUsers(url) {
+  try {
+    const resp = await fetch(url);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.warn(error);
+  }
+}
+
+function showUsersHtml(arr) {
+  outputEl.innerHTML = "";
+  arr.map((obj) => {
+    const divEl = document.createElement("div");
+    divEl.className = "user-info";
+    divEl.innerHTML = `
+        
+        <img src=${obj.avatar_url} alt="userImg" />
+        <p>User login: <span> ${obj.login}</span></p>
+       
+        `;
+    outputEl.append(divEl);
+  });
+}
+
+btnEl.addEventListener("click", async () => {
+  const usersArr = await getUsers(ENDPOINT);
+  showUsersHtml(usersArr);
+});
