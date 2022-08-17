3de74bf4af4350b82d2cd933b6712bffb4afef0f
Arturas <mixmln14@gmail.com>
2022-08-17 23:53:20 +0300

    Task 6 done

--- JS Tasks/Task 6/script.js
+++ JS Tasks/Task 6/script.js
@@ -9,13 +9,33 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+// 1
+
+function getUserAverageAge(arr) {
+  const agesArr = arr.map((obj) => obj.age);
+  return +agesArr.reduce((a, b) => a + b) / agesArr.length;
+  // return +average.toFixed();
+}
+
+const avgAge = getUserAverageAge(users);
+console.log("avgAge ===", avgAge);
+
+// 2
+
+function getUsersNames(arr) {
+  return arr.map((obj) => obj.name);
+}
+
+const namesArr = getUsersNames(users);
+console.log("namesArr ===", namesArr);
