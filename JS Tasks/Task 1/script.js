50746968170c86667b79441d390eba29818a1547
Arturas <mixmln14@gmail.com>
2022-08-17 14:51:48 +0300

    Task 1 done

--- JS Tasks/Task 1/script.js
+++ JS Tasks/Task 1/script.js
@@ -8,3 +8,36 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const outputEl = document.getElementById("output");
+const formEl = document.forms[0];
+const inputEl = document.getElementById("search");
+const submitEl = document.getElementById("submit-btn");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const iValue = +inputEl.value;
+  const svarai = kgToLb(iValue);
+  const gramai = kgToG(iValue);
+  const uncijos = kgToOz(iValue);
+  outputEl.innerHTML = `
+  <p>${iValue} Kg = ${svarai} Lb</p>
+  <p>${iValue} Kg = ${gramai} g</p>
+  <p>${iValue} Kg = ${uncijos} Oz</p>
+  `;
+});
+
+function kgToLb(kg) {
+  const lb = kg * 2.2046;
+  return +lb;
+}
+
+function kgToG(kg) {
+  const g = kg / 0.001;
+  return g;
+}
+
+function kgToOz(kg) {
+  const oz = kg * 35.274;
+  return oz;
+}
