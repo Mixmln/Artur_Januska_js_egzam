5d8e4e5af8f0e0787eed5451f26171c3a1fb4eb8
Arturas <mixmln14@gmail.com>
2022-08-18 09:38:41 +0300

    Task 8 done

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,24 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  this.sum = function (a, b) {
+    return a + b;
+  };
+  this.subtraction = function (a, b) {
+    return a - b;
+  };
+  this.multiplication = function (a, b) {
+    return a * b;
+  };
+  this.division = function (a, b) {
+    return a / b;
+  };
+}
+
+const values = new Calculator();
+console.log("values.sum(5, 10) ===", values.sum(5, 10));
+console.log("values.subtraction(15, 5) ===", values.subtraction(15, 5));
+console.log("values.multiplication(15, 5) ===", values.multiplication(15, 5));
+console.log("values.division(15, 5) ===", values.division(15, 5));
